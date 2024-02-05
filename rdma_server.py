fcae1dd08da0447f76daee1499ac8be72390c7a7
ekfoury <47510793+ekfoury@users.noreply.github.com>
2024-02-05 10:54:56 -0500

    Create rdma_server.py

---
+++ rdma_server.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3.8
+import sys
+
+sys.path.append('..')
+
+from utils.connection import SKT, CM
+from utils.param_parser import parser
+
+from pyverbs.addr import AH, AHAttr, GlobalRoute
+from pyverbs.cq import CQ
+from pyverbs.device import Context
+from pyverbs.enums import *
+from pyverbs.mr import MR
+from pyverbs.pd import PD
+from pyverbs.qp import QP, QPCap, QPInitAttr, QPAttr
+from pyverbs.wr import SGE, RecvWR, SendWR
+import time
+
+RECV_WR = 1
+SEND_WR = 2
+GRH_LENGTH = 40
+
+# TODO: Error handling
+
+args = parser.parse_args()
+
+server = not bool(args['server_ip'])
+
+if args['use_cm']:
+    conn = CM(args['port'], args['server_ip'])
+else:
+    conn = SKT(args['port'], args['server_ip'])
+
+print('-' * 80)
+print(' ' * 25, "Python test for RDMA")
+
+if server:
+    print("Running as server...")
+else:
+    print("Running as client...")
+
+print('-' * 80)
+
+if args['qp_type'] == IBV_QPT_UD and args['operation_type'] != IBV_WR_SEND:
+    print("UD QPs don't support RDMA operations.")
+    conn.close()
+
+conn.handshake()
+
+ctx = Context(name=args['ib_dev'])
+pd = PD(ctx)
+cq = CQ(ctx, 100)
+
+cap = QPCap(max_send_wr=args['tx_depth'], max_recv_wr=args['rx_depth'], max_send_sge=args['sg_depth'],
+            max_recv_sge=args['sg_depth'], max_inline_data=args['inline_size'])
+qp_init_attr = QPInitAttr(qp_type=args['qp_type'], scq=cq, rcq=cq, cap=cap, sq_sig_all=True)
+qp = QP(pd, qp_init_attr)
+
+gid = ctx.query_gid(port_num=1, index=args['gid_index'])
+
+# Handshake to exchange information such as QP Number
+remote_info = conn.handshake(gid=gid, qpn=qp.qp_num)
+
+gr = GlobalRoute(dgid=remote_info['gid'], sgid_index=args['gid_index'])
+ah_attr = AHAttr(gr=gr, is_global=1, port_num=1)
+
+if args['qp_type'] == IBV_QPT_UD:
+    ah = AH(pd, attr=ah_attr)
+    qp.to_rts(QPAttr())
+else:
+    qa = QPAttr()
+    qa.ah_attr = ah_attr
+    qa.dest_qp_num = remote_info['qpn']
+    qa.path_mtu = args['mtu']
+    qa.max_rd_atomic = 1
+    qa.max_dest_rd_atomic = 1
+    qa.qp_access_flags = IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_LOCAL_WRITE
+    if server:
+        qp.to_rtr(qa)
+    else:
+        qp.to_rts(qa)
+
+conn.handshake()
+
+mr_size = args['size']
+if server:
+    if args['qp_type'] == IBV_QPT_UD:   # UD needs more space to store GRH when receiving.
+        mr_size = mr_size + GRH_LENGTH
+    content = 's' * mr_size
+else:
+    content = 'c' * mr_size
+
+mr = MR(pd, mr_size, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ)
+sgl = [SGE(mr.buf, mr.length, mr.lkey)]
+
+print("mr.buf: ", mr.buf)
+print("mr.length: ", mr.length)
+print("mr.lkey: ", mr.lkey)
+
+if args['operation_type'] != IBV_WR_SEND:
+    remote_info = conn.handshake(addr=mr.buf, rkey=mr.rkey)
+
+def read_mr(mr):
+    if args['qp_type'] == IBV_QPT_UD and server:
+        return mr.read(mr.length - GRH_LENGTH, GRH_LENGTH).decode()
+    else:
+        return mr.read(mr.length, 0).decode()    
+
+while(1):
+    time.sleep(1)
+
+conn.handshake()
+conn.close()
+
+print('-' * 80)
